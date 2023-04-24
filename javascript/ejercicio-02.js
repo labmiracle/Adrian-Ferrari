1d467a4a8c9b97ddc9ae8ae9bb47518f5b0c7b4b
Adrian Ferrari <sdadrian@gmail.com>
2023-04-24 15:46:04 -0300

    refactor to a recursive function and change console.log to console.assert

--- javascript/ejercicio-02.js
+++ javascript/ejercicio-02.js
@@ -1,16 +1,12 @@
 function sum(arr, n) {
-  let sum = 0;
-  for (let i = 0; i < n; i++) {
-    if (arr[i]) {
-      sum += arr[i];
-    }
-  }
-  return sum;
+  if (n <= 0) return 0;
+  const suma = (arr[n - 1] ? arr[n - 1] : 0) + sum(arr, n - 1);
+  return suma;
 }
 
 const array = [2, 2, 2, 2, 2, 2];
 
-console.log(sum(array, 0));
-console.log(sum(array, 2));
-console.log(sum(array, 4));
-console.log(sum(array, 8));
+console.assert(sum(array, 0) === 0);
+console.assert(sum(array, 2) === 4);
+console.assert(sum(array, 4) === 8);
+console.assert(sum(array, 8) === 12);
