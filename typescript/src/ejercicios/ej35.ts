9f1d0f6b1882e07bfe6a38752aebfd6e0ac1b0d6
Adrian Ferrari <sdadrian@gmail.com>
2023-05-06 17:38:37 -0300

    add exercise 3.5

---
+++ typescript/src/ejercicios/ej35.ts
@@ -0,0 +1,7 @@
+function layEggs(quantity = 0, color = ""): void {
+    console.log(
+        `[Ejercicio 3.5] Acabas de poner ${quantity} huevos ${color}. Buen trabajo!`
+    );
+}
+
+export default layEggs;
