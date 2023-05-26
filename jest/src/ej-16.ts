b89bde66a7bfe352fde5c5b494b7c036f503847a
Adrian Ferrari <sdadrian@gmail.com>
2023-05-26 14:21:45 -0300

    now properly reconize adyacent 1s on a 2d axis

--- jest/src/ej-16.ts
+++ jest/src/ej-16.ts
@@ -1,12 +1,27 @@
-export function findMaxConsecutiveOnes(matriz: (number | string)[][]): number {
-    if (matriz.find(fila => fila.find(num => num != 1 || 0))) return -1;
-    const rowString = matriz.map(fila => fila.join("")).join(" ");
-    const colString = matriz[0].map((col, i) => matriz.map(row => row[i]).join("")).join(" ");
-    const reg = /1{2,}/g;
-    const matchRow: string[] | null = rowString.match(reg);
-    const matchCol: string[] | null = colString.match(reg);
-    if (!matchRow || !matchCol) return 0;
-    const matchArr = matchRow.concat(matchCol);
-    const longest = matchArr.reduce((acc, curr) => (curr.length > acc.length ? curr : acc));
-    return longest.length;
+type Matrix = (number | string)[][];
+
+function findConsecutiveOne(matrix: Matrix, m: number, n: number): number {
+    const newMatrix = matrix.map(a => [...a]);
+    newMatrix[m][n] = 0;
+    const right: number = 1 + (newMatrix?.[m]?.[n + 1] == 1 ? findConsecutiveOne(newMatrix, m, n + 1) : 0);
+    const left: number = 1 + (newMatrix?.[m]?.[n - 1] == 1 ? findConsecutiveOne(newMatrix, m, n - 1) : 0);
+    const up: number = 1 + (newMatrix?.[m + 1]?.[n] == 1 ? findConsecutiveOne(newMatrix, m + 1, n) : 0);
+    const down: number = 1 + (newMatrix?.[m - 1]?.[n] == 1 ? findConsecutiveOne(newMatrix, m - 1, n) : 0);
+    return Math.max(right, left, up, down);
+}
+
+export function findMaxConsecutiveOnes(matrix: Matrix): number {
+    if (matrix.some(fila => fila.some(num => num != 1 && num != 0))) return -1;
+    const newMatrix = matrix.map(a => [...a]);
+    const arrCount = [0];
+    for (let m = 0; m < newMatrix.length; m++) {
+        for (let n = 0; n < newMatrix[m].length; n++) {
+            if (newMatrix[m][n] == 1) {
+                const maxCount = findConsecutiveOne(newMatrix, m, n);
+                arrCount.push(maxCount);
+            }
+        }
+    }
+
+    return Math.max(...arrCount);
 }
