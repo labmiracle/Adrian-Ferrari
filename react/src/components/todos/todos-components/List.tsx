6ace2c3a83b91f7feb143a30b626bf9019ced56b
Adrian Ferrari <sdadrian@gmail.com>
2023-06-12 16:59:35 -0300

    add no tasks message

--- react/src/components/todos/todos-components/List.tsx
+++ react/src/components/todos/todos-components/List.tsx
@@ -4,6 +4,7 @@ import ListItem from "./ListItem";
 
 export default function List() {
   const { tasks } = useContext(TaskContext);
+  if (tasks.length < 1) return <p className="todo-list-notasks">No hay tareas</p>;
   const tasksListElem = tasks.map((task, i) => {
     return <ListItem {...task} key={task.task + i} />;
   });
--- react/src/components/todos/todos-components/ListContainer.tsx
+++ react/src/components/todos/todos-components/ListContainer.tsx
@@ -5,9 +5,9 @@ export default function ListContainer() {
   return (
     <div className="todo-list-container">
       <>
-        <p className="todo-list-head">ID</p>
-        <p className="todo-list-head">Task</p>
-        <p className="todo-list-head">Remove</p>
+        <p className={"todo-list-head todo-list-id"}>ID</p>
+        <p className={"todo-list-head todo-list-task"}>Task</p>
+        <p className={"todo-list-head todo-list-remove"}>Remove</p>
       </>
       <List />
     </div>
