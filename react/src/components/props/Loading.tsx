2e197dbbdc0b57d4bb9945f251964e8ef482fcad
Adrian Ferrari <sdadrian@gmail.com>
2023-06-12 11:30:27 -0300

    add checkbox and missing exercise description for Props

--- react/src/components/props/Loading.tsx
+++ react/src/components/props/Loading.tsx
@@ -3,8 +3,10 @@ import Text from "./Text";
 import List from "./List";
 import Link from "./Link";
 import "./css/loading.scss";
+import { useState } from "react";
 
 export default function Loading({ isLoading }: { isLoading: boolean }) {
+  const [openInNewTab, setOpenInNewTab] = useState(true);
   const title = "Necesito partir en componentes todo esto";
   const text = `Para ello puedo usar React que me permitirá reutilizar todos esos
   componentes. Para ello tengo que:`;
@@ -14,17 +16,25 @@ export default function Loading({ isLoading }: { isLoading: boolean }) {
     "Usarlos, o mejor dicho, reusarlos en React",
   ];
 
+  function handleCheckbox() {
+    setOpenInNewTab(!openInNewTab);
+  }
+
   return (
     <div className="loading">
       {isLoading ? (
         <h1>Loading...</h1>
       ) : (
-        <>
+        <div className="loading-parts">
           <Title title={title} />
           <Text text={text} />
           <List items={items} />
-          <Link openInNewTab={true} />
-        </>
+          <div className="props-link">
+            <Link openInNewTab={true} />
+            <label htmlFor="props-checkbox">abrir en otra tab?</label>
+            <input id="props-checkbox" type="checkbox" checked={openInNewTab} onChange={handleCheckbox} />
+          </div>
+        </div>
       )}
     </div>
   );
--- react/src/components/props/Props.tsx
+++ react/src/components/props/Props.tsx
@@ -6,10 +6,14 @@ export default function Props() {
   const [isLoading, setLoading] = useState(true);
   return (
     <div className="props">
+      <p>
+        Crear un componente llamado Loading que si su prop show es verdadera muestre sus hijos. Si es falsa muestre un
+        mensaje: Loading...
+      </p>
       <Loading isLoading={isLoading} />
       {isLoading && (
         <button className="load-btn" onClick={() => setLoading(() => false)}>
-          Cargar
+          Show
         </button>
       )}
     </div>
