31e4c799c84f7b4e401fc4da6b7c2da9b763cda6
Adrian Ferrari <sdadrian@gmail.com>
2023-06-20 15:05:21 -0300

    fix: added alt text

--- minecraft-crafting-app/src/components/helpers/getImage.tsx
+++ minecraft-crafting-app/src/components/helpers/getImage.tsx
@@ -5,5 +5,5 @@ import { APosition, CraftablesTypes, IngredientsTypes } from "../../items/items"
 export function getImage<T extends IngredientsTypes | CraftablesTypes>(name: T, array: APosition<T>, key: number) {
   const { x, y } = getPosition(name, array);
 
-  return <img className="craft-img" src={items} style={{ objectPosition: `-${x}px -${y}px` }} key={key} />;
+  return <img className="craft-img" src={items} style={{ objectPosition: `-${x}px -${y}px` }} key={key} alt={name} />;
 }
