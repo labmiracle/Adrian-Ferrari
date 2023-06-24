7c60c92d5d28b44d60204216ba16fc190ed9e9c9
Adrian Ferrari <sdadrian@gmail.com>
2023-06-23 21:20:42 -0300

    feat: add svg image source as component

---
+++ minecraft-crafting-app/src/components/Arrow.tsx
@@ -0,0 +1,7 @@
+export default function Arrow() {
+  return (
+    <svg fill="currentColor" stroke="currentColor" version="1.1" id="Capa_1" viewBox="0 0 490 490">
+      <polygon points="268.814,217.052 0,217.052 0,262.989 268.814,262.989 268.814,399.88 490,245 268.814,90.12 " />
+    </svg>
+  );
+}
